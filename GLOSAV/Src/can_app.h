9da8e20a8186570c03e890bf18ddf7567be9ef55
nowhard <nowhard@mail.ru>
2015-11-27 13:21:04 +0300

    Добавлены комментарии к структуре CAN

--- GLOSAV/Src/can_app.h
+++ GLOSAV/Src/can_app.h
@@ -10,16 +10,17 @@ void CAN_App_Init(void);
 #pragma pack(1) 
 typedef	struct 
 {
-	uint16_t 	vehicleSpeed;
-	uint8_t 	calcLoad;
-	uint32_t 	totalFuelCons;
-	uint8_t  	fuelLevelRel;
-	uint16_t	engSpeed;
-	uint16_t  axleWeight[AXLE_NUM];
-	uint32_t 	calcTotalEngTime;
-	uint16_t 	serviceDist;
-	uint16_t 	fuelRate;
+	uint16_t 	vehicleSpeed;//скорость тс 1/256 км/ч на бит, значение FMS 10250
+	uint8_t 	calcLoad; //нагрузка на двигатель 1% на бит, значение FMS 255
+	uint32_t 	totalFuelCons;//расход топлива расчетный 0.5л на бит, значение FMS 224778
+	uint8_t  	fuelLevelRel;// относительный уровень топлива в баке 0.4% на бит, значение FMS 25
+	uint16_t	engSpeed;//обороты двигателя 0.125 об/сек на бит, значение FMS 6400
+	uint16_t  	axleWeight[AXLE_NUM];//нагрузка на оси, 0.5 кг на бит, значение FMS не определено
+	uint32_t 	calcTotalEngTime;// моточасы расчетные, 0.05 часа на бит, значение FMS 132413
+	uint16_t 	serviceDist;//Пробег до ТО, 5 км на бит,  значение FMS 36833
+	uint16_t 	fuelRate;// расход топлива, 0,05 л/ч на бит, параметр не передается FMS
 } stCANFMSParams;
+
 #pragma pack() 
 
 #endif
