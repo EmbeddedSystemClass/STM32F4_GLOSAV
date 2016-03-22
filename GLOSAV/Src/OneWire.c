fa306cf0537bf6ff3ab8309d7d001249d34d2379
nowhard <nowhard@mail.ru>
2016-03-22 11:23:25 +0300

    iButton проверена, тайминги на прерывании

--- GLOSAV/Src/OneWire.c
+++ GLOSAV/Src/OneWire.c
@@ -111,6 +111,7 @@ uint8_t OneWire_Reset(OneWire_t* OneWireStruct) {
 	/* Return value of presence pulse, 0 = OK, 1 = ERROR */
 	return i;
 }
+
 #ifndef ONEWIRE_RW_HARDWARE_TIMER
 void OneWire_WriteBit(OneWire_t* OneWireStruct, uint8_t bit) 
 {
@@ -190,231 +191,6 @@ uint8_t OneWire_ReadByte(OneWire_t* OneWireStruct) {
 	return byte;
 }
 
-uint8_t OneWire_First(OneWire_t* OneWireStruct) {
-	/* Reset search values */
-	OneWire_ResetSearch(OneWireStruct);
-
-	/* Start with searching */
-	return OneWire_Search(OneWireStruct, ONEWIRE_CMD_SEARCHROM);
-}
-
-uint8_t OneWire_Next(OneWire_t* OneWireStruct) {
-   /* Leave the search state alone */
-   return OneWire_Search(OneWireStruct, ONEWIRE_CMD_SEARCHROM);
-}
-
-void OneWire_ResetSearch(OneWire_t* OneWireStruct) {
-	/* Reset the search state */
-	OneWireStruct->LastDiscrepancy = 0;
-	OneWireStruct->LastDeviceFlag = 0;
-	OneWireStruct->LastFamilyDiscrepancy = 0;
-}
-
-uint8_t OneWire_Search(OneWire_t* OneWireStruct, uint8_t command) {
-	uint8_t id_bit_number;
-	uint8_t last_zero, rom_byte_number, search_result;
-	uint8_t id_bit, cmp_id_bit;
-	uint8_t rom_byte_mask, search_direction;
-
-	/* Initialize for search */
-	id_bit_number = 1;
-	last_zero = 0;
-	rom_byte_number = 0;
-	rom_byte_mask = 1;
-	search_result = 0;
-
-	// if the last call was not the last one
-	if (!OneWireStruct->LastDeviceFlag) {
-		// 1-Wire reset
-		if (OneWire_Reset(OneWireStruct)) {
-			/* Reset the search */
-			OneWireStruct->LastDiscrepancy = 0;
-			OneWireStruct->LastDeviceFlag = 0;
-			OneWireStruct->LastFamilyDiscrepancy = 0;
-			return 0;
-		}
-
-		// issue the search command 
-		OneWire_WriteByte(OneWireStruct, command);  
-
-		// loop to do the search
-		do {
-			// read a bit and its complement
-			id_bit = OneWire_ReadBit(OneWireStruct);
-			cmp_id_bit = OneWire_ReadBit(OneWireStruct);
-
-			// check for no devices on 1-wire
-			if ((id_bit == 1) && (cmp_id_bit == 1)) {
-				break;
-			} else {
-				// all devices coupled have 0 or 1
-				if (id_bit != cmp_id_bit) {
-					search_direction = id_bit;  // bit write value for search
-				} else {
-					// if this discrepancy if before the Last Discrepancy
-					// on a previous next then pick the same as last time
-					if (id_bit_number < OneWireStruct->LastDiscrepancy) {
-						search_direction = ((OneWireStruct->ROM_NO[rom_byte_number] & rom_byte_mask) > 0);
-					} else {
-						// if equal to last pick 1, if not then pick 0
-						search_direction = (id_bit_number == OneWireStruct->LastDiscrepancy);
-					}
-					
-					// if 0 was picked then record its position in LastZero
-					if (search_direction == 0) {
-						last_zero = id_bit_number;
-
-						// check for Last discrepancy in family
-						if (last_zero < 9) {
-							OneWireStruct->LastFamilyDiscrepancy = last_zero;
-						}
-					}
-				}
-
-				// set or clear the bit in the ROM byte rom_byte_number
-				// with mask rom_byte_mask
-				if (search_direction == 1) {
-					OneWireStruct->ROM_NO[rom_byte_number] |= rom_byte_mask;
-				} else {
-					OneWireStruct->ROM_NO[rom_byte_number] &= ~rom_byte_mask;
-				}
-				
-				// serial number search direction write bit
-				OneWire_WriteBit(OneWireStruct, search_direction);
-
-				// increment the byte counter id_bit_number
-				// and shift the mask rom_byte_mask
-				id_bit_number++;
-				rom_byte_mask <<= 1;
-
-				// if the mask is 0 then go to new SerialNum byte rom_byte_number and reset mask
-				if (rom_byte_mask == 0) {
-					//docrc8(ROM_NO[rom_byte_number]);  // accumulate the CRC
-					rom_byte_number++;
-					rom_byte_mask = 1;
-				}
-			}
-		} while (rom_byte_number < 8);  // loop until through all ROM bytes 0-7
-
-		// if the search was successful then
-		if (!(id_bit_number < 65)) {
-			// search successful so set LastDiscrepancy,LastDeviceFlag,search_result
-			OneWireStruct->LastDiscrepancy = last_zero;
-
-			// check for last device
-			if (OneWireStruct->LastDiscrepancy == 0) {
-				OneWireStruct->LastDeviceFlag = 1;
-			}
-
-			search_result = 1;
-		}
-	}
-
-	// if no device found then reset counters so next 'search' will be like a first
-	if (!search_result || !OneWireStruct->ROM_NO[0]) {
-		OneWireStruct->LastDiscrepancy = 0;
-		OneWireStruct->LastDeviceFlag = 0;
-		OneWireStruct->LastFamilyDiscrepancy = 0;
-		search_result = 0;
-	}
-
-	return search_result;
-}
-
-int OneWire_Verify(OneWire_t* OneWireStruct) {
-	unsigned char rom_backup[8];
-	int i,rslt,ld_backup,ldf_backup,lfd_backup;
-
-	// keep a backup copy of the current state
-	for (i = 0; i < 8; i++)
-	rom_backup[i] = OneWireStruct->ROM_NO[i];
-	ld_backup = OneWireStruct->LastDiscrepancy;
-	ldf_backup = OneWireStruct->LastDeviceFlag;
-	lfd_backup = OneWireStruct->LastFamilyDiscrepancy;
-
-	// set search to find the same device
-	OneWireStruct->LastDiscrepancy = 64;
-	OneWireStruct->LastDeviceFlag = 0;
-
-	if (OneWire_Search(OneWireStruct, ONEWIRE_CMD_SEARCHROM)) {
-		// check if same device found
-		rslt = 1;
-		for (i = 0; i < 8; i++) {
-			if (rom_backup[i] != OneWireStruct->ROM_NO[i]) {
-				rslt = 1;
-				break;
-			}
-		}
-	} else {
-		rslt = 0;
-	}
-
-	// restore the search state 
-	for (i = 0; i < 8; i++) {
-		OneWireStruct->ROM_NO[i] = rom_backup[i];
-	}
-	OneWireStruct->LastDiscrepancy = ld_backup;
-	OneWireStruct->LastDeviceFlag = ldf_backup;
-	OneWireStruct->LastFamilyDiscrepancy = lfd_backup;
-
-	// return the result of the verify
-	return rslt;
-}
-
-void OneWire_TargetSetup(OneWire_t* OneWireStruct, uint8_t family_code) {
-   uint8_t i;
-
-	// set the search state to find SearchFamily type devices
-	OneWireStruct->ROM_NO[0] = family_code;
-	for (i = 1; i < 8; i++) {
-		OneWireStruct->ROM_NO[i] = 0;
-	}
-	
-	OneWireStruct->LastDiscrepancy = 64;
-	OneWireStruct->LastFamilyDiscrepancy = 0;
-	OneWireStruct->LastDeviceFlag = 0;
-}
-
-void OneWire_FamilySkipSetup(OneWire_t* OneWireStruct) {
-	// set the Last discrepancy to last family discrepancy
-	OneWireStruct->LastDiscrepancy = OneWireStruct->LastFamilyDiscrepancy;
-	OneWireStruct->LastFamilyDiscrepancy = 0;
-
-	// check for end of list
-	if (OneWireStruct->LastDiscrepancy == 0) {
-		OneWireStruct->LastDeviceFlag = 1;
-	}
-}
-
-uint8_t OneWire_GetROM(OneWire_t* OneWireStruct, uint8_t index) {
-	return OneWireStruct->ROM_NO[index];
-}
-
-void OneWire_Select(OneWire_t* OneWireStruct, uint8_t* addr) {
-	uint8_t i;
-	OneWire_WriteByte(OneWireStruct, ONEWIRE_CMD_MATCHROM);
-	
-	for (i = 0; i < 8; i++) {
-		OneWire_WriteByte(OneWireStruct, *(addr + i));
-	}
-}
-
-void OneWire_SelectWithPointer(OneWire_t* OneWireStruct, uint8_t *ROM) {
-	uint8_t i;
-	OneWire_WriteByte(OneWireStruct, ONEWIRE_CMD_MATCHROM);
-	
-	for (i = 0; i < 8; i++) {
-		OneWire_WriteByte(OneWireStruct, *(ROM + i));
-	}	
-}
-
-void OneWire_GetFullROM(OneWire_t* OneWireStruct, uint8_t *firstIndex) {
-	uint8_t i;
-	for (i = 0; i < 8; i++) {
-		*(firstIndex + i) = OneWireStruct->ROM_NO[i];
-	}
-}
-
 uint8_t OneWire_CRC8(uint8_t *addr, uint8_t len) {
 	uint8_t crc = 0, inbyte, i, mix;
 	
@@ -429,20 +205,9 @@ uint8_t OneWire_CRC8(uint8_t *addr, uint8_t len) {
 			inbyte >>= 1;
 		}
 	}
-	
-	/* Return calculated CRC */
 	return crc;
 }
 
-void OneWire_ReadROM(OneWire_t* OneWireStruct)
-{
-	OneWire_Reset(OneWireStruct);
-	OneWire_WriteByte(OneWireStruct,ONEWIRE_CMD_READROM);
-	for(uint8_t i=0; i<8; i++) 
-	{
-			OneWireStruct->ROM_NO[i]=OneWire_ReadByte(OneWireStruct);
-	}
-}
 
 uint8_t temperature[2];
 
@@ -521,15 +286,20 @@ static void OneWire_Task(void *pvParameters)
 	{  
 		//OneWire_DS18b20(&OneWireStruct);
 		result=OneWire_Read_iButton(&OneWireStruct,iButtonData);
-		//if(OneWire_Read_iButton(&OneWireStruct,iButtonData)==0);
 		if(result==0)
 		{
 			for(i=0;i<8;i++)
 			{			
-				MBHoldingRegParams.params.iButtonID[i]=iButtonData[i];
+					MBHoldingRegParams.params.iButtonID[i]=iButtonData[i];
 			}
 		}
-		//OneWire_ReadROM(&OneWireStruct);
+		else
+		{
+			for(i=0;i<8;i++)
+			{			
+					MBHoldingRegParams.params.iButtonID[i]=0;
+			}
+		}
 		vTaskDelay(ONEWIRE_READ_PERIOD);
 	}
 }
@@ -554,8 +324,6 @@ void TIM7_IRQHandler(void)
 		{
 				OneWire_Output();
 				ONEWIRE_LOW();
-				//htim7.Init.Period = 15;
-				//HAL_TIM_Base_Init(&htim7);
 				TIM7->ARR=15;
 			  HAL_TIM_Base_Start_IT(&htim7);
 			
@@ -583,10 +351,7 @@ void TIM7_IRQHandler(void)
 		
 		case OW_STATE_WRITE_0:
 		{
-				//OneWire_StrongPullUp_Off();	
 				ONEWIRE_LOW();
-//				htim7.Init.Period = 45;
-//				HAL_TIM_Base_Init(&htim7);
 				TIM7->ARR=45;
 			  HAL_TIM_Base_Start_IT(&htim7);
 				ow_state=OW_STATE_TIMESLOT_END;
@@ -596,9 +361,6 @@ void TIM7_IRQHandler(void)
 		case OW_STATE_WRITE_1:
 		{
 				ONEWIRE_HIGH();
-				//OneWire_StrongPullUp_On();	
-//				htim7.Init.Period = 45;
-//				HAL_TIM_Base_Init(&htim7);
 				TIM7->ARR=45;
 			  HAL_TIM_Base_Start_IT(&htim7);	
 				ow_state=OW_STATE_TIMESLOT_END;			
@@ -610,8 +372,6 @@ void TIM7_IRQHandler(void)
 				OneWire_Input();
 				ONEWIRE_HIGH();
 				ow_bit=HAL_GPIO_ReadPin(in_1_wire_in_GPIO_Port, in_1_wire_in_Pin);
-//				htim7.Init.Period = 45;
-//				HAL_TIM_Base_Init(&htim7);
 				TIM7->ARR=45;
 			  HAL_TIM_Base_Start_IT(&htim7);	
 				ow_state=OW_STATE_TIMESLOT_END;		
@@ -622,15 +382,8 @@ void TIM7_IRQHandler(void)
 		{
 				OneWire_Output();
 				ONEWIRE_HIGH();
-				//OneWire_StrongPullUp_Off();	
-			  //ONEWIRE_LOW();
 				ow_state=OW_STATE_IDLE;
 		}
 		break;
 	}
-	
-	
-
-//	HAL_GPIO_TogglePin(in_1_wire_in_GPIO_Port, in_1_wire_in_Pin);
-//	HAL_TIM_Base_Start_IT(&htim7);
 }
